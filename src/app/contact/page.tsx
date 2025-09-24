7bf8dace98613e75f2842ab936913f2af814ff54
GANGADHAR D <gangadhard2317@gmail.com>
2025-09-24 18:26:53 +0000

    I NEED MANY MORE ACCESSBILITIES IN THE CONTACT

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,4 @@
+
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
